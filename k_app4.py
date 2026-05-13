f26d5d6455ed2cf07347104dec48792a829b98cc
paradisoilpapa <paradiso.kumamoto@gmail.com>
2026-05-13 14:41:03 +0900

    Update k_app4.py

--- k_app4.py
+++ k_app4.py
@@ -4389,34 +4389,6 @@ try:
                 scenario_top_count=int(_scenario_top_count_map.get(_car, 0) or 0),
                 is_h_lead_thirdplus=_is_h_lead_thirdplus,
             )
-
-
-
-
-                
-            try:
-                _zmap = globals().get("LINE_ZONE_MAP", {})
-
-                _line_key_for_car = ""
-                _members_for_car = []
-
-                for _gid, _mem in _line_def.items():
-                    _mem2 = [int(x) for x in _mem]
-                    if _car in _mem2:
-                        _members_for_car = _mem2
-                        _line_key_for_car = "".join(str(x) for x in _mem2)
-                        break
-
-                if (
-                    isinstance(_zmap, dict)
-                    and _zmap.get(_line_key_for_car) == "順流"
-                    and len(_members_for_car) >= 3
-                    and _role == "thirdplus"
-                    and _car in _members_for_car[2:]
-                ):
-                    
-
-            except Exception:
                 
         
 
